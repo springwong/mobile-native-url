630e50e56e8b88cbd4a708c3c5185410d00a6c2c
Spring <development.spring@gmail.com>
2021-05-09 18:00:56 +0800

    added url get cases

--- js/url.test.js
+++ js/url.test.js
@@ -1,6 +1,6 @@
 const url = require('url');
 
-test('WHATWG URL standard', () => {
+test('WHATWG URL standard - auth url', () => {
     const testUrl = new URL('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
     expect(testUrl.protocol).toBe('https:');
     expect(testUrl.username).toBe('user');
@@ -13,4 +13,79 @@ test('WHATWG URL standard', () => {
     expect(testUrl.search).toBe('?query=string');
     expect(testUrl.searchParams.get('query')).toBe('string');
     expect(testUrl.hash).toBe('#hash');
+});
+
+test('WHATWG URL standard - host:port', () => {
+    const testUrl = new URL('https://sub.example.com:8080/p/a/t/h?query=string#hash');
+    expect(testUrl.protocol).toBe('https:');
+    expect(testUrl.username).toBe('');
+    expect(testUrl.password).toBe('');
+    expect(testUrl.host).toBe('sub.example.com:8080');
+    expect(testUrl.hostname).toBe('sub.example.com');
+    expect(testUrl.port).toBe('8080');
+    expect(testUrl.origin).toBe('https://sub.example.com:8080');
+    expect(testUrl.pathname).toBe('/p/a/t/h');
+    expect(testUrl.search).toBe('?query=string');
+    expect(testUrl.searchParams.get('query')).toBe('string');
+    expect(testUrl.hash).toBe('#hash');
+});
+
+test('WHATWG URL standard - host', () => {
+    const testUrl = new URL('https://sub.example.com/p/a/t/h?query=string#hash');
+    expect(testUrl.protocol).toBe('https:');
+    expect(testUrl.username).toBe('');
+    expect(testUrl.password).toBe('');
+    expect(testUrl.host).toBe('sub.example.com');
+    expect(testUrl.hostname).toBe('sub.example.com');
+    expect(testUrl.port).toBe('');
+    expect(testUrl.origin).toBe('https://sub.example.com');
+    expect(testUrl.pathname).toBe('/p/a/t/h');
+    expect(testUrl.search).toBe('?query=string');
+    expect(testUrl.searchParams.get('query')).toBe('string');
+    expect(testUrl.hash).toBe('#hash');
+});
+
+test('WHATWG URL standard - host no hash', () => {
+    const testUrl = new URL('https://sub.example.com/p/a/t/h?query=string');
+    expect(testUrl.protocol).toBe('https:');
+    expect(testUrl.username).toBe('');
+    expect(testUrl.password).toBe('');
+    expect(testUrl.host).toBe('sub.example.com');
+    expect(testUrl.hostname).toBe('sub.example.com');
+    expect(testUrl.port).toBe('');
+    expect(testUrl.origin).toBe('https://sub.example.com');
+    expect(testUrl.pathname).toBe('/p/a/t/h');
+    expect(testUrl.search).toBe('?query=string');
+    expect(testUrl.searchParams.get('query')).toBe('string');
+    expect(testUrl.hash).toBe('');
+});
+
+test('WHATWG URL standard - host domain only', () => {
+    const testUrl = new URL('https://sub.example.com?query=string');
+    expect(testUrl.protocol).toBe('https:');
+    expect(testUrl.username).toBe('');
+    expect(testUrl.password).toBe('');
+    expect(testUrl.host).toBe('sub.example.com');
+    expect(testUrl.hostname).toBe('sub.example.com');
+    expect(testUrl.port).toBe('');
+    expect(testUrl.origin).toBe('https://sub.example.com');
+    expect(testUrl.pathname).toBe('/');
+    expect(testUrl.search).toBe('?query=string');
+    expect(testUrl.searchParams.get('query')).toBe('string');
+    expect(testUrl.hash).toBe('');
+});
+
+test('WHATWG URL standard - host no query', () => {
+    const testUrl = new URL('https://sub.example.com');
+    expect(testUrl.protocol).toBe('https:');
+    expect(testUrl.username).toBe('');
+    expect(testUrl.password).toBe('');
+    expect(testUrl.host).toBe('sub.example.com');
+    expect(testUrl.hostname).toBe('sub.example.com');
+    expect(testUrl.port).toBe('');
+    expect(testUrl.origin).toBe('https://sub.example.com');
+    expect(testUrl.pathname).toBe('/');
+    expect(testUrl.search).toBe('');
+    expect(testUrl.searchParams.get('query')).toBe(null);
+    expect(testUrl.hash).toBe('');
 });
